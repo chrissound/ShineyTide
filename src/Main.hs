ba5c6ef494743ba0e33b55c392f8d021a2909fa9
Chris Stryczynski <chris@soundshare.co.uk>
2018-06-30 15:54:31 +0000

    it works woohoo

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import Sound.OSC.FD
+import Control.Monad
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  startServ 1235 >>= print
+
+startServ :: Int -> IO ()
+startServ port = do
+  scServer <- openUDP "127.0.0.1" 57120
+  x <- udpServer "127.0.0.1" port
+  forever $ do
+    m <- waitImmediate x
+    sendOSC scServer $ m
+    print m
